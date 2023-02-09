abb766443c5494fa10416333de0ddf020e094115
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2023-02-10 02:09:43 +0800

    feat: add icons to skills in about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,28 +1,189 @@
 import style from '@/components/About/style.module.css';
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaBootstrap,
+  FaSass,
+  FaGitAlt,
+  FaReact,
+  FaNodeJs,
+  FaFigma,
+  FaTrello,
+  FaGithub,
+  FaGitlab,
+} from 'react-icons/fa';
+import {
+  SiTailwindcss,
+  SiVite,
+  SiWebpack,
+  SiRedux,
+  SiChakraui,
+  SiJavascript,
+  SiTypescript,
+  SiExpress,
+  SiMongodb,
+  SiJest,
+  SiJirasoftware,
+  SiTestinglibrary,
+} from 'react-icons/si';
 
 const About = () => {
   return (
     <section id='about' className={style.container}>
       <h3 className={style.sectionTitle}>About me</h3>
-      <div>
-        <p className={style.text}>
-          I am <span className={style.primaryColor}>Ronaldo Lipata</span>,
-          graduated with a Bachelor's degree in IT. Aside from Web Development,
-          I am also a Blockchain enthusiast.
-        </p>
-        <p className={style.text}>
-          Having a passion in Web Development makes me feel elated every time I
-          completed projects.
-        </p>
-        <p className={style.text}>
-          Here are my skills and technologies that I use: <br />
-          <span className={style.primaryColor}>
-            HTML, CSS, Tailwind, Bootstrap, SASS, JavaScript, Git, Vite,
-            Webpack, React, Redux, Chakra UI, TypeScript, Node, Express, REST
-            API, MongoDB, Figma UI/UX, TDD, React Testing, Jest, RTL, Jira,
-            Trello
-          </span>
-        </p>
+      <div className={style.subContainer}>
+        <div className={style.aboutTextContainer}>
+          <p className={style.text}>
+            I am <span className={style.primaryColor}>Ronaldo Lipata</span>,
+            graduated with a Bachelor's degree in IT. Aside from Web
+            Development, I am also a Blockchain enthusiast.
+          </p>
+          <p className={style.text}>
+            Having a passion in Web Development makes me feel elated every time
+            I completed projects.
+          </p>
+        </div>
+
+        <div className={style.skillsMainContainer}>
+          <div className={style.skillsSubContainer}>
+            <h3 className={style.primaryColor}>Front-End Technologies</h3>
+            <div className={style.skillsContainer}>
+              <span className={style.iconFlex}>
+                HTML
+                <FaHtml5 />
+              </span>
+              <span className={style.iconFlex}>
+                CSS
+                <FaCss3Alt />
+              </span>
+              <span className={style.iconFlex}>
+                Tailwind
+                <SiTailwindcss />
+              </span>
+              <span className={style.iconFlex}>
+                Bootstrap
+                <FaBootstrap />
+              </span>
+              <span className={style.iconFlex}>
+                SASS
+                <FaSass />
+              </span>
+              <span className={style.iconFlex}>
+                JavaScript
+                <SiJavascript />
+              </span>
+              <span className={style.iconFlex}>
+                TypeScript
+                <SiTypescript />
+              </span>
+              <span className={style.iconFlex}>
+                React
+                <FaReact />
+              </span>
+              <span className={style.iconFlex}>
+                Redux
+                <SiRedux />
+              </span>
+              <span className={style.iconFlex}>
+                Chakra UI
+                <SiChakraui />
+              </span>
+              <span className={style.iconFlex}>
+                Figma
+                <FaFigma />
+              </span>
+            </div>
+          </div>
+          <div className={style.skillsSubContainer}>
+            <h3 className={style.primaryColor}>Back-End Technologies</h3>
+            <div className={style.skillsContainer}>
+              <span className={style.iconFlex}>
+                JavaScript
+                <SiJavascript />
+              </span>
+              <span className={style.iconFlex}>
+                TypeScript
+                <SiTypescript />
+              </span>
+              <span className={style.iconFlex}>
+                Node JS
+                <FaNodeJs />
+              </span>
+              <span className={style.iconFlex}>
+                Express JS
+                <SiExpress />
+              </span>
+              <span className={style.iconFlex}>
+                MongoDB
+                <SiMongodb />
+              </span>
+              <span>Mongoose</span>
+            </div>
+          </div>
+          <div className={style.skillsSubContainerFlexRow}>
+            <div className={style.skillsSubContainer}>
+              <h3 className={style.primaryColor}>Build Tools / Bundler</h3>
+              <div className={style.skillsContainer}>
+                <span className={style.iconFlex}>
+                  Vite
+                  <SiVite />
+                </span>
+                <span className={style.iconFlex}>
+                  Webpack
+                  <SiWebpack />
+                </span>
+              </div>
+            </div>
+
+            <div className={style.skillsSubContainer}>
+              <h3 className={style.primaryColor}>DevOps / Version Control</h3>
+              <div className={style.skillsContainer}>
+                <span className={style.iconFlex}>
+                  Git
+                  <FaGitAlt />
+                </span>
+                <span className={style.iconFlex}>
+                  GitHub
+                  <FaGithub />
+                </span>
+                <span className={style.iconFlex}>
+                  GitLab
+                  <FaGitlab />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className={style.skillsSubContainerFlexRow}>
+            <div className={style.skillsSubContainer}>
+              <h3 className={style.primaryColor}>Project Management</h3>
+              <div className={style.skillsContainer}>
+                <span className={style.iconFlex}>
+                  Jira
+                  <SiJirasoftware />
+                </span>
+                <span className={style.iconFlex}>
+                  Trello
+                  <FaTrello />
+                </span>
+              </div>
+            </div>
+
+            <div className={style.skillsSubContainer}>
+              <h3 className={style.primaryColor}>Testing</h3>
+              <div className={style.skillsContainer}>
+                <span className={style.iconFlex}>
+                  Jest
+                  <SiJest />
+                </span>
+                <span className={style.iconFlex}>
+                  RTL
+                  <SiTestinglibrary />
+                </span>
+                <span>TDD</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
