ccc3743c397fc264000089cd0dc5d42a2e7c4075
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2023-07-21 13:25:28 +0800

    refac: remove unused import from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
-import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
